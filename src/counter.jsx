331a17931deb48681b379b5e667bfadee0de6df3
Kenny <kawhyte@gmail.com>
2020-01-14 14:40:09 -0800

    Binding events

--- src/counter.jsx
+++ src/counter.jsx
@@ -4,13 +4,35 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags:['tag1', 'tag2','tag3']
+    tags: ["tag1", "tag2", "tag3"]
   };
+//  constructor(){
 
+//     super();
+//     this.handleIncrement= this.handleIncrement.bind(this);
+//  }
   styles = {
     fontSize: 51,
     fontWeight: "bold"
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>No tags</p>;
+    } else {
+      return (
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+  handleIncrement = () => {
+    console.log("incremented!", this);
+  }
 
   render() {
     return (
@@ -19,21 +41,23 @@ class Counter extends Component {
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Incrament</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Incrament
+        </button>
 
-        <ul>
-    {this.state.tags.map( tag =><li key={tag}>{tag}</li>)}
-            
-        </ul>
+        {this.renderTags()}
       </div>
     );
   }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 ";
-        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
+  }
 
   formatCount() {
     return this.state.count === 0 ? "Zero" : this.state.count;
