f0c8906cdb10ea28a8ef958df9985e7f146fcf1a
Kenny <kawhyte@gmail.com>
2020-01-14 14:13:08 -0800

    Dynamic Rendering

--- src/counter.jsx
+++ src/counter.jsx
@@ -3,23 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: 'https://picsum.photos/200'
+    imageUrl: "https://picsum.photos/200"
   };
 
-  styles ={
-      fontSize:100,
-      fontWeight:'bold'
-  }
+  styles = {
+    fontSize: 40,
+    fontWeight: "bold"
+  };
 
   render() {
     return (
       <div>
-          <img src={this.state.imageUrl} alt=""/>
-        <span style ={this.styles} className= "badge badge-primary m-2">{this.formatCount()}</span>
-        <button className= "btn btn-secondary btn-sm">Incrament</button>
+        {/* <img src={this.state.imageUrl} alt=""/> */}
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Incrament</button>
       </div>
     );
   }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        return classes;
+    }
 
   formatCount() {
     return this.state.count === 0 ? "Zero" : this.state.count;
