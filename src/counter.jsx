59041ea3d1d42149f15b2cb7002930ef559a699a
Kenny <kawhyte@gmail.com>
2020-01-14 13:56:33 -0800

    Styles

--- src/counter.jsx
+++ src/counter.jsx
@@ -2,19 +2,27 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imageUrl: 'https://picsum.photos/200'
   };
+
+  styles ={
+      fontSize:100,
+      fontWeight:'bold'
+  }
+
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Incrament</button>
+          <img src={this.state.imageUrl} alt=""/>
+        <span style ={this.styles} className= "badge badge-primary m-2">{this.formatCount()}</span>
+        <button className= "btn btn-secondary btn-sm">Incrament</button>
       </div>
     );
   }
 
   formatCount() {
-    return this.state.count === 0 ? <h1> Zero</h1> : this.state.count;
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
