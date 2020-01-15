571f3e0855c49bd5286c2dc7e32cb1c6cc92c5df
Kenny <kawhyte@gmail.com>
2020-01-15 13:40:51 -0800

    Delete

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,27 +1,38 @@
-import React, { Component } from 'react';
-import Counter from './counter'
+import React, { Component } from "react";
+import Counter from "./counter";
 
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 10 },
+      { id: 2, value: 100 },
+      { id: 3, value: 2 },
+      { id: 4, value: 1 },
+      { id: 5, value: 0 }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <h1>Counters</h1>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            // value={counter.value}
+            // id={counter.id}
+            counter={counter}
+          ></Counter>
+        ))}
+      </div>
+    );
+  }
 
-class  Counters extends Component {
-    state = { counters:[
-
-        {id:10, value:10},
-        {id:2, value:100},
-        {id:3, value:2},
-        {id:4, value:1},
-        {id:5, value:0}
-    ] }
-    render() { 
-        return (<div>
-            <h1>Counters</h1>
-            {this.state.counters.map(counter => <Counter key = {counter.id} value ={counter.value} id ={counter.id} >
-
-           
-            </Counter>)}
-            </div>  
-             
-            );
-    }
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+    console.log("Deleted!!!!!!", counters);
+  };
 }
- 
+
 export default Counters;
