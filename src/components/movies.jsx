8114bb570d4823236f47780ce80a7393bef8c13c
Kenny <kawhyte@gmail.com>
2020-01-15 10:14:11 -0800

    Delete movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,25 +4,41 @@ import { getMovies } from "../services/fakeMovieService";
 class Movies extends Component {
   state = { movies: getMovies() };
 
-  handleDelete = movie => {};
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    console.log(movie);
+    this.setState({ movies: movies });
+    console.log(movies);
+  };
+
   render() {
     return (
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">Title</th>
             <th scope="col">Genre</th>
             <th scope="col">Stock</th>
             <th scope="col">Rate</th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-small"
+                >
+                  {" "}
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
